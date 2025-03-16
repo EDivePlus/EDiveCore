0e0afc6029fe53622b6b145d2a9ee0e1831dc156
František Holubec <frantisek.holubec@gmail.com>
2025-03-16 20:31:52 +0100

    Persistent context utils

---
+++ OdinExtensions/Editor/PersistentContextUtils.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Serialization;
+using UnityEditor;
+
+namespace EDIVE.OdinExtensions.Editor
+{
+    public static class PersistentContextUtils
+    {
+        public static GlobalPersistentContext<TValue> Get<TKey1, TValue>(TKey1 alphaKey, TValue defaultValue) => 
+            PersistentContext.Get(GetHash(alphaKey), defaultValue);
+        
+        public static GlobalPersistentContext<TValue> Get<TKey1, TKey2, TValue>(TKey1 alphaKey, TKey2 betaKey, TValue defaultValue) => 
+            PersistentContext.Get(GetHash(alphaKey), GetHash(betaKey), defaultValue);
+
+        public static GlobalPersistentContext<TValue> Get<TKey1, TKey2, TKey3, TValue>(TKey1 alphaKey, TKey2 betaKey, TKey3 gammaKey, TValue defaultValue) => 
+            PersistentContext.Get(GetHash(alphaKey), GetHash(betaKey), GetHash(gammaKey), defaultValue);
+
+        public static GlobalPersistentContext<TValue> Get<TKey1, TKey2, TKey3, TKey4, TValue>(TKey1 alphaKey, TKey2 betaKey, TKey3 gammaKey, TKey4 deltaKey, TValue defaultValue) => 
+            PersistentContext.Get(GetHash(alphaKey), GetHash(betaKey), GetHash(gammaKey), GetHash(deltaKey), defaultValue);
+
+        private static int GetHash<TKey>(TKey key) => key is Type typeKey ? TwoWaySerializationBinder.Default.BindToName(typeKey).GetHashCode() : key.GetHashCode();
+    }
+
+    [Serializable]
+    [InlineProperty]
+    [HideReferenceObjectPicker]
+    public sealed class ValuePersistentContext<TValue>
+    {
+        private GlobalPersistentContext<TValue> _context;
+
+        public ValuePersistentContext(GlobalPersistentContext<TValue> context)
+        {
+            _context = context;
+        }
+
+        [ShowInInspector]
+        [HideLabel]
+        public TValue Value
+        {
+            get => _context != null ? _context.Value : default;
+            set
+            {
+                if (_context != null) _context.Value = value;
+            }
+        }
+    }
+
+    [Serializable]
+    [InlineProperty]
+    [HideReferenceObjectPicker]
+    public sealed class AssetPersistentContext<TValue> where TValue : UnityEngine.Object
+    {
+        private GlobalPersistentContext<string> _context;
+
+        public AssetPersistentContext(GlobalPersistentContext<string> context)
+        {
+            _context = context;
+        }
+
+        [ShowInInspector]
+        [HideLabel]
+        public TValue Value
+        {
+            get => AssetDatabase.LoadAssetAtPath<TValue>(AssetDatabase.GUIDToAssetPath(_context?.Value));
+            set
+            {
+                if (_context != null) _context.Value = AssetDatabase.AssetPathToGUID(AssetDatabase.GetAssetPath(value));
+            }
+        }
+    }
+}
