6e8e3d5745b1877d7c7a8b69350c7e0636b14b41
František Holubec <frantisek.holubec@gmail.com>
2025-03-16 20:34:47 +0100

    Directory cleaner

---
+++ EditorUtils/DirectoryCleaner/DirectoryCleanerUtility.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EDIVE.NativeUtils;
+using UnityEditor;
+using UnityEngine;
+
+namespace EDIVE.EditorUtils.DirectoryCleaner
+{
+    public static class DirectoryCleanerUtility
+    {
+        private const string CLEAR_ON_SAVE_PREFS_KEY = "DirectoryCleanerUtility_ClearOnSave";
+
+        public static bool CleanOnEditorInitialized
+        {
+            get => EditorPrefs.GetBool(CLEAR_ON_SAVE_PREFS_KEY, false); 
+            set => EditorPrefs.SetBool(CLEAR_ON_SAVE_PREFS_KEY, value);
+        }
+
+        [InitializeOnLoadMethod]
+        private static void OnEditorLoaded()
+        {
+            if (!CleanOnEditorInitialized) return;
+
+            var emptyDirs = GetEmptyDirectories();
+            if (!emptyDirs.Any()) 
+                return;
+            
+            DeleteDirectories(emptyDirs);
+            Debug.Log( "[DirectoryCleaner] Cleared Empty Directories" );
+        }
+
+        public static void DeleteDirectories(IEnumerable<DirectoryInfo> directories)
+        {
+            var failedPaths = new List<string>();
+            AssetDatabase.MoveAssetsToTrash(directories.Select(d => PathUtility.GetProjectRelativePath(d.FullName)).ToArray(), failedPaths);
+        }
+        
+        public static void DeleteDirectory(DirectoryInfo directory)
+        {
+            AssetDatabase.MoveAssetToTrash(PathUtility.GetProjectRelativePath(directory.FullName));
+        }
+
+        public static List<DirectoryInfo> GetEmptyDirectories()
+        {
+            var emptyDirs = new List<DirectoryInfo>();
+            var assetsDir = new DirectoryInfo(Application.dataPath);
+            CheckEmptyDirectory(assetsDir, ref emptyDirs);
+            return emptyDirs;
+        }
+
+        private static bool CheckEmptyDirectory(DirectoryInfo root, ref List<DirectoryInfo> emptyDirs)
+        {
+            try
+            {
+                var subDirs = root.GetDirectories();
+                var allSubDirsEmpty = true;
+                foreach (var subDir in subDirs)
+                {
+                    if (!CheckEmptyDirectory(subDir, ref emptyDirs))
+                    {
+                        allSubDirsEmpty = false;
+                    }
+                }
+
+                if (!allSubDirsEmpty || HasDirectoryAnyFiles(root))
+                    return false;
+            
+                if(!emptyDirs.Contains(root))
+                    emptyDirs.Add(root);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static bool HasDirectoryAnyFiles(DirectoryInfo dirInfo)
+        {
+            try
+            {
+                return dirInfo.EnumerateFileSystemInfos("*.*").Any(IsNonMetaFile);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        private static bool IsNonMetaFile(FileSystemInfo file)
+        {
+            return file.Extension != ".meta";
+        }
+    }
+}
---
+++ EditorUtils/DirectoryCleaner/DirectoryCleanerWindow.cs
@@ -0,0 +1,136 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using EDIVE.NativeUtils;
+using EDIVE.OdinExtensions;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEngine;
+
+namespace EDIVE.EditorUtils.DirectoryCleaner
+{
+    public class DirectoryCleanerWindow : OdinEditorWindow
+    {
+        [ShowInInspector]
+        [LabelWidth(150)]
+        private bool ClearOnEditorLoaded { get => DirectoryCleanerUtility.CleanOnEditorInitialized; set => DirectoryCleanerUtility.CleanOnEditorInitialized = value; }
+
+        private bool AnyEmptyDirectories => EmptyDirectories != null && EmptyDirectories.Any();
+        private bool AnySelectedDirectories => SelectedDirectories != null && SelectedDirectories.Any();
+
+        [ShowInInspector]
+        [PropertyOrder(10)]
+        [CustomValueDrawer(nameof(CustomDirectoryDrawer))]
+        [ListDrawerSettings(ShowFoldout = false, IsReadOnly = true)]
+        private List<DirectoryInfo> EmptyDirectories { get; set; } = new List<DirectoryInfo>();
+
+        private HashSet<DirectoryInfo> SelectedDirectories { get; set; } = new HashSet<DirectoryInfo>();
+
+        [Button]
+        private void Refresh()
+        {
+            EmptyDirectories = DirectoryCleanerUtility.GetEmptyDirectories();
+            RefreshSelected();
+
+            if (EmptyDirectories.Any())
+            {
+                RemoveNotification();
+            }
+            else
+            {
+                ShowNotification(new GUIContent("No Empty Directory"));
+            }
+        }
+
+        [PropertySpace]
+        [Button]
+        [HorizontalGroup("Delete", Order = 15)]
+        [EnableIf(nameof(AnySelectedDirectories))]
+        private void DeleteSelected()
+        {
+            DirectoryCleanerUtility.DeleteDirectories(SelectedDirectories);
+            EmptyDirectories = DirectoryCleanerUtility.GetEmptyDirectories();
+            RefreshSelected();
+        }
+        
+        [PropertySpace]
+        [Button]
+        [HorizontalGroup("Delete", Order = 15)]
+        [EnableIf(nameof(AnyEmptyDirectories))]
+        private void DeleteAll()
+        {
+            DirectoryCleanerUtility.DeleteDirectories(EmptyDirectories);
+            EmptyDirectories = DirectoryCleanerUtility.GetEmptyDirectories();
+            RefreshSelected();
+        }
+
+        [MenuItem("NoxE/Directory Cleaner")]
+        public static void ShowWindow()
+        {
+            GetWindow<DirectoryCleanerWindow>();
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+            titleContent = new GUIContent("Directory Cleaner", FontAwesomeEditorIcons.FolderGearSolid.Active);
+        }
+
+        private DirectoryInfo CustomDirectoryDrawer(DirectoryInfo value)
+        {
+            if (value == null || !value.Exists)
+            {
+                GUILayout.Label("Invalid path info");
+                return value;
+            }
+
+            EditorGUILayout.BeginHorizontal();
+
+            EditorGUI.BeginChangeCheck();
+            var wasSelected = SelectedDirectories.Contains(value);
+            var isSelected = EditorGUILayout.Toggle(wasSelected, GUILayout.Width(25));
+            if (EditorGUI.EndChangeCheck() && wasSelected != isSelected)
+            {
+                if (isSelected)
+                    SelectedDirectories.Add(value);
+                else
+                    SelectedDirectories.Remove(value);
+            }
+
+            var relativePath = PathUtility.GetProjectRelativePath(value.FullName);
+            SirenixEditorFields.TextField(relativePath);
+            if (GUILayout.Button("Ping", GUILayout.ExpandWidth(false)))
+            {
+                var targetObj = AssetDatabase.LoadAssetAtPath<Object>(relativePath);
+                EditorGUIUtility.PingObject(targetObj);
+                Selection.activeObject = targetObj;
+            }
+
+            if (GUILayout.Button("Show In Explorer", GUILayout.ExpandWidth(false)))
+            {
+                EditorUtility.RevealInFinder(value.FullName.ConvertPathSeparator());
+            }
+
+            var previousColor = GUI.backgroundColor;
+            GUI.backgroundColor = Color.red;
+            GUILayout.Space(8);
+            if (GUILayout.Button("Delete", GUILayout.ExpandWidth(false)))
+            {
+                DirectoryCleanerUtility.DeleteDirectory(value);
+                EmptyDirectories = DirectoryCleanerUtility.GetEmptyDirectories();
+                RefreshSelected();
+            }
+
+            GUI.backgroundColor = previousColor;
+            EditorGUILayout.EndHorizontal();
+            return value;
+        }
+
+        private void RefreshSelected()
+        {
+            SelectedDirectories = new HashSet<DirectoryInfo>(EmptyDirectories.Where(d => SelectedDirectories.Any(s => d.FullName == s.FullName)));
+        }
+    }
+}
