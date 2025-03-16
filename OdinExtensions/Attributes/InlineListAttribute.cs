bc1f8add8f9480089f698b0b3d28ae8c3f125ab4
František Holubec <frantisek.holubec@gmail.com>
2025-03-16 20:31:37 +0100

    Inline list attribute

---
+++ OdinExtensions/Attributes/InlineListAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Author: František Holubec
+// Created: 16.03.2025
+
+using System;
+using System.Diagnostics;
+using Sirenix.OdinInspector;
+
+namespace EDIVE.OdinExtensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    [Conditional("UNITY_EDITOR")]
+    [DontApplyToListElements]
+    public class InlineListAttribute : Attribute
+    {
+        public string ElementSuffixGetter;
+
+        public InlineListAttribute() { }
+    }
+}
---
+++ OdinExtensions/Editor/Drawers/InlineListAttributeDrawer.cs
@@ -0,0 +1,53 @@
+﻿// Author: František Holubec
+// Created: 16.03.2025
+
+using EDIVE.OdinExtensions.Attributes;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector.Editor.ValueResolvers;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+
+namespace EDIVE.OdinExtensions.Editor.Drawers
+{
+    public class InlineListAttributeDrawer
+    {
+        public class SimpleListAttributeDrawer : OdinAttributeDrawer<InlineListAttribute>
+        {
+            private const string INDEX_ARGUMENT_ID = "index";
+            private ValueResolver<string> _elementSuffixResolver;
+
+            protected override void Initialize()
+            {
+                base.Initialize();
+                _elementSuffixResolver = ValueResolver.GetForString(Property, Attribute.ElementSuffixGetter, new NamedValue(INDEX_ARGUMENT_ID, typeof(int)));
+            }
+
+            protected override bool CanDrawAttributeProperty(InspectorProperty property)
+            {
+                return property.ChildResolver is IOrderedCollectionResolver;
+            }
+
+            protected override void DrawPropertyLayout(GUIContent label)
+            {
+                ValueResolver.DrawErrors(_elementSuffixResolver);
+                SirenixEditorGUI.BeginHorizontalPropertyLayout(label);
+                for (var index = 0; index < Property.Children.Count; index++)
+                {
+                    var child = Property.Children[index];
+                    child.Draw(null);
+                    if (Attribute.ElementSuffixGetter != null && !_elementSuffixResolver.HasError)
+                    {
+                        GUIHelper.PushGUIEnabled(true);
+                        _elementSuffixResolver.Context.NamedValues.Set(INDEX_ARGUMENT_ID, index);
+                        var suffix = _elementSuffixResolver.GetValue();
+                        GUI.Label(GUILayoutUtility.GetLastRect().HorizontalPadding(0.0f, 8f), suffix, SirenixGUIStyles.RightAlignedGreyMiniLabel);
+                        GUIHelper.PopGUIEnabled();
+                    }
+                }
+
+                SirenixEditorGUI.EndHorizontalPropertyLayout();
+            }
+        }
+    }
+}
