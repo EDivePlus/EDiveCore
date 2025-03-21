80a26ed7b82ae53052cb0360694e2c5eaba681f1
František Holubec <frantisek.holubec@gmail.com>
2025-03-21 11:41:24 +0100

    Defines utility

---
+++ EditorUtils/DefinesConfigWindow.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using EDIVE.NativeUtils;
+using EDIVE.OdinExtensions;
+using EDIVE.OdinExtensions.Attributes;
+using Newtonsoft.Json;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace EDIVE.EditorUtils
+{
+    public class DefinesConfigWindow : OdinEditorWindow
+    {
+        [Searchable(FilterOptions = SearchFilterOptions.ValueToString)]
+        [EnhancedTableList(AlwaysExpanded = true, ShowPaging = false, OnTitleBarGUI = nameof(OnRecordsToolbarGUI))]
+        [SerializeField]
+        private List<DefineRecord> _Records;
+
+        public static IEnumerable<SupportedBuildTarget> SupportedBuildTargets => EnumUtils.GetValues<SupportedBuildTarget>();
+
+        [MenuItem("Tools/Defines Config")]
+        public static void OpenFontReplaceUtility()
+        {
+            GetWindow<DefinesConfigWindow>();
+        }
+
+        private void OnRecordsToolbarGUI()
+        {
+            if (SirenixEditorGUI.ToolbarButton(EditorIcons.Refresh))
+            {
+                RefreshRecords();
+            }
+        }
+
+        [OnInspectorInit]
+        private void RefreshRecords()
+        {
+            var dict = new Dictionary<string, List<SupportedBuildTarget>>();
+            foreach (var target in SupportedBuildTargets)
+            {
+                var namedTarget = GetNamedTarget(target);
+                DefinesUtility.GetScriptingDefineSymbols(namedTarget, out var defines);
+                foreach (var define in defines)
+                {
+                    if (!dict.TryGetValue(define, out var targetGroups))
+                    {
+                        targetGroups = new List<SupportedBuildTarget>();
+                        dict.Add(define, targetGroups);
+                    }
+                    targetGroups.Add(target);
+                }
+            }
+            _Records = dict.Select(d => new DefineRecord(d.Key, d.Value)).ToList();
+        }
+
+        [Button]
+        private void ApplyRecords()
+        {
+            foreach (var target in SupportedBuildTargets)
+            {
+                var namedTarget = GetNamedTarget(target);
+                var defines = _Records.Where(r => r.BuildTargets.Contains(target)).Select(r => r.Define).ToList();
+                DefinesUtility.SetScriptingDefineSymbols(namedTarget, defines);
+            }
+            AssetDatabase.Refresh();
+            AssetDatabase.SaveAssets();
+        }
+
+        private static EditorIcon GetEditorIcon(SupportedBuildTarget buildTarget)
+        {
+            return buildTarget switch
+            {
+                SupportedBuildTarget.Standalone => FontAwesomeEditorIcons.DesktopSolid,
+                SupportedBuildTarget.Server => FontAwesomeEditorIcons.ServerSolid,
+                SupportedBuildTarget.Android => CustomEditorIcons.Android,
+                SupportedBuildTarget.IOS => FontAwesomeEditorIcons.Apple,
+                _ => FontAwesomeEditorIcons.SquareQuestionSolid
+            };
+        }
+
+        private static NamedBuildTarget GetNamedTarget(SupportedBuildTarget buildTarget)
+        {
+            return buildTarget switch
+            {
+                SupportedBuildTarget.Standalone => NamedBuildTarget.Standalone,
+                SupportedBuildTarget.Server => NamedBuildTarget.Server,
+                SupportedBuildTarget.Android => NamedBuildTarget.Android,
+                SupportedBuildTarget.IOS => NamedBuildTarget.iOS,
+                _ => NamedBuildTarget.Unknown
+            };
+        }
+
+        public enum SupportedBuildTarget
+        {
+            Standalone,
+            Server,
+            Android,
+            IOS
+        }
+
+        [Serializable]
+        [JsonObject(MemberSerialization.OptIn)]
+        public class DefineRecord
+        {
+            [SerializeField]
+            [JsonProperty("Define")]
+            private string _Define;
+
+            [FormerlySerializedAs("_BuildTargetGroups")]
+            [HideInInspector]
+            [SerializeField]
+            [JsonProperty("BuildTargets")]
+            private List<SupportedBuildTarget> _BuildTargets = new();
+
+            public string Define => _Define;
+            public List<SupportedBuildTarget> BuildTargets => _BuildTargets;
+
+            public DefineRecord() { }
+            public DefineRecord(string define, List<SupportedBuildTarget> targets)
+            {
+                _Define = define;
+                _BuildTargets = targets;
+            }
+
+            [EnhancedTableColumn(100)]
+            [VerticalGroup("Platforms", Order = -1)]
+            [OnInspectorGUI]
+            private void DrawBuildTargets(InspectorProperty property)
+            {
+                GUILayout.BeginHorizontal();
+                GUILayout.Space(4);
+                foreach (var targetGroup in SupportedBuildTargets)
+                {
+                    var rect = GUILayoutUtility.GetRect(18, 18, SirenixGUIStyles.Button, GUILayoutOptions.ExpandWidth(false).Width(18));
+                    var enabled = _BuildTargets.Contains(targetGroup);
+                    var editorIcon = GetEditorIcon(targetGroup);
+                    var icon = enabled ? editorIcon.Highlighted : editorIcon.Inactive;
+                    GUIHelper.PushContentColor(enabled ? ColorTools.Green : ColorTools.White);
+                    if (SirenixEditorGUI.IconButton(rect, icon, targetGroup.ToString()))
+                    {
+                        if (enabled) _BuildTargets.Remove(targetGroup);
+                        else _BuildTargets.Add(targetGroup);
+                        property.MarkSerializationRootDirty();
+                    }
+                    GUIHelper.PopContentColor();
+                    GUILayout.Space(2);
+                }
+                GUILayout.Space(2);
+                GUILayout.EndHorizontal();
+            }
+
+            public bool HasBuildTarget(SupportedBuildTarget targetGroup)
+            {
+                return _BuildTargets.Contains(targetGroup);
+            }
+
+            public override string ToString() => Define;
+        }
+    }
+}
--- EditorUtils/DefinesUtility.cs
+++ EditorUtils/DefinesUtility.cs
@@ -1,8 +1,6 @@
 ﻿#if UNITY_EDITOR
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using UnityEditor;
 using UnityEditor.Build;
 
@@ -10,19 +8,39 @@ namespace EDIVE.EditorUtils
 {
     public static class DefinesUtility
     {
-        public static void SetDefines(IEnumerable<string> defines, bool enabled, IEnumerable<BuildTargetGroup> groups = null)
+        private static readonly NamedBuildTarget[] VALID_NAMED_BUILD_TARGETS =
         {
-            if (enabled) AddDefines(defines, groups);
-            else RemoveDefines(defines, groups);
+            NamedBuildTarget.Unknown,
+            NamedBuildTarget.Standalone,
+            NamedBuildTarget.Server,
+            NamedBuildTarget.iOS,
+            NamedBuildTarget.Android,
+            NamedBuildTarget.WebGL,
+            NamedBuildTarget.WindowsStoreApps,
+            NamedBuildTarget.PS4,
+            NamedBuildTarget.XboxOne,
+            NamedBuildTarget.tvOS,
+            NamedBuildTarget.VisionOS,
+            NamedBuildTarget.NintendoSwitch,
+            NamedBuildTarget.Stadia,
+            NamedBuildTarget.LinuxHeadlessSimulation,
+            NamedBuildTarget.EmbeddedLinux,
+            NamedBuildTarget.QNX,
+        };
+
+        public static void SetDefines(IEnumerable<string> defines, bool enabled, IEnumerable<NamedBuildTarget> targets = null)
+        {
+            if (enabled) AddDefines(defines, targets);
+            else RemoveDefines(defines, targets);
         }
 
-        public static void AddDefines(IEnumerable<string> defines, IEnumerable<BuildTargetGroup> groups = null)
+        public static void AddDefines(IEnumerable<string> defines, IEnumerable<NamedBuildTarget> targets = null)
         {
-            groups ??= GetValidBuildTargetGroups();
+            targets ??= VALID_NAMED_BUILD_TARGETS;
             var definesList = defines.ToList();
-            foreach (var group in groups)
+            foreach (var group in targets)
             {
-                GetScriptingDefineSymbolsForGroup(group, out var currentDefines);
+                GetScriptingDefineSymbols(group, out var currentDefines);
 
                 foreach (var define in definesList)
                 {
@@ -31,17 +49,17 @@ namespace EDIVE.EditorUtils
 
                     currentDefines.Add(define);
                 }
-                SetScriptingDefineSymbolsForGroup(group, currentDefines);
+                SetScriptingDefineSymbols(group, currentDefines);
             }
         }
 
-        public static void RemoveDefines(IEnumerable<string> defines, IEnumerable<BuildTargetGroup> groups = null)
+        public static void RemoveDefines(IEnumerable<string> defines, IEnumerable<NamedBuildTarget> targets = null)
         {
-            groups ??= GetValidBuildTargetGroups();
+            targets ??= VALID_NAMED_BUILD_TARGETS;
             var definesList = defines.ToList();
-            foreach (var group in groups)
+            foreach (var group in targets)
             {
-                GetScriptingDefineSymbolsForGroup(group, out var currentDefines);
+                GetScriptingDefineSymbols(group, out var currentDefines);
 
                 foreach (var define in definesList)
                 {
@@ -50,75 +68,65 @@ namespace EDIVE.EditorUtils
 
                     currentDefines.Remove(define);
                 }
-                SetScriptingDefineSymbolsForGroup(group, currentDefines);
+                SetScriptingDefineSymbols(group, currentDefines);
             }
         }
 
-        public static void SetDefine(string define, bool enabled, IEnumerable<BuildTargetGroup> groups = null)
+        public static void SetDefine(string define, bool enabled, IEnumerable<NamedBuildTarget> targets = null)
         {
-            if (enabled) AddDefine(define, groups);
-            else RemoveDefine(define, groups);
+            if (enabled) AddDefine(define, targets);
+            else RemoveDefine(define, targets);
         }
 
-        public static bool AddDefine(string define, IEnumerable<BuildTargetGroup> groups = null)
+        public static bool AddDefine(string define, IEnumerable<NamedBuildTarget> targets = null)
         {
-            groups ??= GetValidBuildTargetGroups();
+            targets ??= VALID_NAMED_BUILD_TARGETS;
             var added = false;
 
-            foreach (var group in groups)
+            foreach (var group in targets)
             {
-                GetScriptingDefineSymbolsForGroup(group, out var currentDefines);
+                GetScriptingDefineSymbols(group, out var currentDefines);
 
                 if (currentDefines.Contains(define))
                     continue;
 
                 currentDefines.Add(define);
-                SetScriptingDefineSymbolsForGroup(group, currentDefines);
+                SetScriptingDefineSymbols(group, currentDefines);
                 added = true;
             }
 
             return added;
         }
 
-        public static bool RemoveDefine(string define, IEnumerable<BuildTargetGroup> groups = null)
+        public static bool RemoveDefine(string define, IEnumerable<NamedBuildTarget> targets = null)
         {
-            groups ??= GetValidBuildTargetGroups();
+            targets ??= VALID_NAMED_BUILD_TARGETS;
             var removed = false;
 
-            foreach (var group in groups)
+            foreach (var group in targets)
             {
-                GetScriptingDefineSymbolsForGroup(group, out var currentDefines);
+                GetScriptingDefineSymbols(group, out var currentDefines);
 
                 if (!currentDefines.Contains(define))
                     continue;
 
                 currentDefines.Remove(define);
-                SetScriptingDefineSymbolsForGroup(group, currentDefines);
+                SetScriptingDefineSymbols(group, currentDefines);
                 removed = true;
             }
 
             return removed;
         }
 
-        public static void GetScriptingDefineSymbolsForGroup(BuildTargetGroup targetGroup, out List<string> defines)
+        public static void GetScriptingDefineSymbols(NamedBuildTarget target, out List<string> defines)
         {
-            var namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(targetGroup);
-            PlayerSettings.GetScriptingDefineSymbols(namedBuildTarget, out var definesArray);
+            PlayerSettings.GetScriptingDefineSymbols(target, out var definesArray);
             defines = definesArray.ToList();
         }
 
-        public static void SetScriptingDefineSymbolsForGroup(BuildTargetGroup targetGroup, List<string> defines)
+        public static void SetScriptingDefineSymbols(NamedBuildTarget target, List<string> defines)
         {
-            var namedBuildTarget = NamedBuildTarget.FromBuildTargetGroup(targetGroup);
-            PlayerSettings.SetScriptingDefineSymbols(namedBuildTarget, defines.ToArray());
-        }
-
-        private static IEnumerable<BuildTargetGroup> GetValidBuildTargetGroups()
-        {
-            return Enum.GetValues(typeof(BuildTargetGroup))
-                .Cast<BuildTargetGroup>()
-                .Where(group => group != BuildTargetGroup.Unknown &&
-                                typeof(BuildTargetGroup).GetField(group.ToString()).GetCustomAttribute<ObsoleteAttribute>() == null);
+            PlayerSettings.SetScriptingDefineSymbols(target, defines.ToArray());
         }
     }
 }
