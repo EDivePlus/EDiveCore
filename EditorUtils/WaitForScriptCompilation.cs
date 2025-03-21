dca480451fbe8657c1f083e0a133cda40933e192
František Holubec <frantisek.holubec@gmail.com>
2025-03-21 10:28:29 +0100

    WaitForScipt compilation yield instruction

---
+++ EditorUtils/WaitForScriptCompilation.cs
@@ -0,0 +1,37 @@
+﻿// Author: František Holubec
+// Created: 17.03.2025
+
+using UnityEditor.Compilation;
+using UnityEngine;
+
+namespace EDIVE.EditorUtils
+{
+    public class WaitForScriptCompilation : CustomYieldInstruction
+    {
+        private bool _isCompilationRequested;
+        private bool _isCompilationDone;
+
+        public CompilerMessage[] CompilationMessages { get; private set; }
+
+        public override bool keepWaiting
+        {
+            get
+            {
+                if (!_isCompilationRequested)
+                {
+                    _isCompilationRequested = true;
+                    CompilationPipeline.assemblyCompilationFinished += OnCompilationFinished;
+                    CompilationPipeline.RequestScriptCompilation();
+                }
+                return !_isCompilationDone;
+            }
+        }
+
+        private void OnCompilationFinished(string assembly, CompilerMessage[] messages)
+        {
+            CompilationPipeline.assemblyCompilationFinished -= OnCompilationFinished;
+            CompilationMessages = messages;
+            _isCompilationDone = true;
+        }
+    }
+}
