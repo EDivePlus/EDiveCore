052081788544a00565af9dd94e41b896dbb8daeb
František Holubec <frantisek.holubec@gmail.com>
2025-04-03 10:01:22 +0200

    Toolbar extender editor only

--- External/ToolbarExtensions/ToolbarExtender.cs
+++ External/ToolbarExtensions/ToolbarExtender.cs
@@ -1,5 +1,4 @@
-﻿#if UNITY_EDITOR
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEditor;
@@ -172,4 +171,3 @@ namespace EDIVE.External.ToolbarExtensions
         }
     }
 }
-#endif
