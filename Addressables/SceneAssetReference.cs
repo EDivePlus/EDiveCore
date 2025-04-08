2afe8b45245a79cd2ff7a18845e71af6fba858d1
František Holubec <frantisek.holubec@gmail.com>
2025-04-08 18:18:02 +0200

    Scene asset reference

---
+++ Addressables/SceneAssetReference.cs
@@ -0,0 +1,44 @@
+﻿// Author: František Holubec
+// Created: 08.04.2025
+
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace EDIVE.Addressables
+{
+    [System.Serializable]
+    public class SceneAssetReference : AssetReference
+    {
+        public SceneAssetReference() { }
+        public SceneAssetReference(string guid) : base(guid) { }
+
+        /// <inheritdoc/>
+        public override bool ValidateAsset(Object obj)
+        {
+#if UNITY_EDITOR
+            var type = obj.GetType();
+            return typeof(UnityEditor.SceneAsset).IsAssignableFrom(type);
+#else
+            return false;
+#endif
+        }
+
+        /// <inheritdoc/>
+        public override bool ValidateAsset(string path)
+        {
+#if UNITY_EDITOR
+            var type = UnityEditor.AssetDatabase.GetMainAssetTypeAtPath(path);
+            return typeof(UnityEditor.SceneAsset).IsAssignableFrom(type);
+#else
+            return path.EndsWith(".unity");
+#endif
+        }
+
+#if UNITY_EDITOR
+        // Type-specific override of parent editorAsset.  Used by the editor to represent the asset referenced.
+        // ReSharper disable once InconsistentNaming
+        public new UnityEditor.SceneAsset editorAsset => (UnityEditor.SceneAsset) base.editorAsset;
+#endif
+    }
+
+}
