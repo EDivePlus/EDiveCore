58316e89afccc3a92b56c9703bbabec9e2f31183
Radim Holub <radikholub1@seznam.cz>
2025-05-15 12:29:42 +0200

    Simulator VR compatibility

--- XRTools/Controls/ControlsManager.cs
+++ XRTools/Controls/ControlsManager.cs
@@ -1,3 +1,6 @@
+using System;
+using Cysharp.Threading.Tasks;
+using EDIVE.AppLoading;
 using EDIVE.Core.Services;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -7,7 +10,7 @@ using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
 
 namespace EDIVE.XRTools.Controls
 {
-    public class ControlsManager : AServiceBehaviour<ControlsManager>
+    public class ControlsManager : ALoadableServiceBehaviour<ControlsManager>
     {
         [SerializeField]
         private ControlsHandler _DesktopControls;
@@ -23,18 +26,21 @@ namespace EDIVE.XRTools.Controls
         private TeleportationProvider _TeleportationProvider;
 
         [ShowInInspector]
-        public bool XREnabled => UnityEngine.XR.XRSettings.enabled || XRInteractionSimulator.instance;
+        public bool XREnabled => UnityEngine.XR.XRSettings.enabled || XRDeviceSimulator.instance || XRInteractionSimulator.instance;
 
         public XRInteractionManager InteractionManager => _InteractionManager;
         public TeleportationProvider TeleportationProvider => _TeleportationProvider;
 
         public ControlsHandler Controls => XREnabled ? _HeadsetControls : _DesktopControls;
+        
+        
 
-        protected override void Awake()
+        protected override async UniTask LoadRoutine(Action<float> progressCallback)
         {
-            base.Awake();
+            await UniTask.Yield();
             _DesktopControls.gameObject.SetActive(!XREnabled);
             _HeadsetControls.gameObject.SetActive(XREnabled);
         }
     }
+    
 }
