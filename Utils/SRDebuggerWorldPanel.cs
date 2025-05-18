0f8974cfdfdc327b376c2629fd6ec89d8965356f
František Holubec <frantisek.holubec@gmail.com>
2025-05-18 15:20:39 +0200

    SR debugger utils

---
+++ Utils/SRDebuggerWorldPanel.cs
@@ -0,0 +1,64 @@
+﻿// Author: František Holubec
+// Created: 18.05.2025
+
+#if SR_DEBUGGER && XR_INTERACTION_TOOLKIT
+using Cysharp.Threading.Tasks;
+using EDIVE.NativeUtils;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.Interaction.Toolkit.UI;
+
+namespace EDIVE.Utils
+{
+    public class SRDebuggerWorldPanel : MonoBehaviour
+    {
+        [SerializeField]
+        private RectTransform _ParentRect;
+
+        [SerializeField]
+        private TrackedDeviceGraphicRaycaster _XRRaycaster;
+
+        private RectTransform _panelRect;
+
+        private void Awake()
+        {
+            _panelRect = SRDebug.Instance.EnableWorldSpaceMode();
+            _panelRect.SetParent(_ParentRect, false);
+            _panelRect.SetToFillParent();
+        }
+
+        // ReSharper disable once Unity.IncorrectMethodSignature
+        private async UniTaskVoid Start()
+        {
+            await UniTask.WaitForEndOfFrame();
+            if (_XRRaycaster)
+            {
+                foreach (var raycaster in _panelRect.GetComponentsInChildren<GraphicRaycaster>(true))
+                {
+                    var xrRaycaster = raycaster.GetOrAddComponent<TrackedDeviceGraphicRaycaster>();
+                    CopyTrackedDeviceGraphicRaycasterData(xrRaycaster, _XRRaycaster);
+                }
+            }
+        }
+
+        private void OnEnable()
+        {
+            SRDebug.Instance?.ShowDebugPanel();
+        }
+
+        private void OnDisable()
+        {
+            SRDebug.Instance?.HideDebugPanel();
+        }
+
+        private static void CopyTrackedDeviceGraphicRaycasterData(TrackedDeviceGraphicRaycaster target, TrackedDeviceGraphicRaycaster original)
+        {
+            target.ignoreReversedGraphics = original.ignoreReversedGraphics;
+            target.blockingMask = original.blockingMask;
+            target.checkFor2DOcclusion = original.checkFor2DOcclusion;
+            target.checkFor3DOcclusion = original.checkFor3DOcclusion;
+            target.raycastTriggerInteraction = original.raycastTriggerInteraction;
+        }
+    }
+}
+#endif
