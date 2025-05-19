3a9e348846527d9fb58a29cc87c395a7f583cba8
František Holubec <frantisek.holubec@gmail.com>
2025-05-19 09:57:05 +0200

    Signal - await once

--- External/Signals/Signal.cs
+++ External/Signals/Signal.cs
@@ -16,50 +16,50 @@
 
 /**
  * @class strange.extensions.signal.impl.Signal
- * 
+ *
  * This is actually a series of classes defining the Base concrete form for all Signals.
- * 
+ *
  * Signals are a type-safe approach to communication that essentially replace the
  * standard EventDispatcher model. Signals can be injected/mapped just like any other
  * object -- as Singletons, as instances, or as values. Signals can even be mapped
  * across Contexts to provide an effective and type-safe way of communicating
  * between the parts of your application.
- * 
+ *
  * Signals in Strange use the Action Class as the underlying mechanism for type safety.
  * Unity's C# implementation currently allows up to FOUR parameters in an Action, therefore
  * SIGNALS ARE LIMITED TO FOUR PARAMETERS. If you require more than four, consider
  * creating a value object to hold additional values.
- * 
+ *
  * Examples:
 
-		//BASIC SIGNAL CREATION/DISPATCH
-		//Create a new signal
-		Signal signalWithNoParameters = new Signal();
-		//Add a listener
-		signalWithNoParameters.AddListener(callbackWithNoParameters);
-		//This would throw a compile-time error
-		signalWithNoParameters.AddListener(callbackWithOneParameter);
-		//Dispatch
-		signalWithNoParameters.Dispatch();
-		//Remove the listener
-		signalWithNoParameters.RemoveListener(callbackWithNoParameters);
+        //BASIC SIGNAL CREATION/DISPATCH
+        //Create a new signal
+        Signal signalWithNoParameters = new Signal();
+        //Add a listener
+        signalWithNoParameters.AddListener(callbackWithNoParameters);
+        //This would throw a compile-time error
+        signalWithNoParameters.AddListener(callbackWithOneParameter);
+        //Dispatch
+        signalWithNoParameters.Dispatch();
+        //Remove the listener
+        signalWithNoParameters.RemoveListener(callbackWithNoParameters);
 
-		//SIGNAL WITH PARAMETERS
-		//Create a new signal with two parameters
-		Signal<int, string> signal = new Signal<int, string>();
-		//Add a listener
-		signal.AddListener(callbackWithParamsIntAndString);
-		//Add a listener for the duration of precisely one Dispatch
-		signal.AddOnce(anotherCallbackWithParamsIntAndString);
-		//These all throw compile-time errors
-		signal.AddListener(callbackWithParamsStringAndInt);
-		signal.AddListener(callbackWithOneParameter);
-		signal.AddListener(callbackWithNoParameters);
-		//Dispatch
-		signal.Dispatch(42, "zaphod");
-		//Remove the first listener. The listener added by AddOnce has been automatically removed.
-		signal.RemoveListener(callbackWithParamsIntAndString);
- * 
+        //SIGNAL WITH PARAMETERS
+        //Create a new signal with two parameters
+        Signal<int, string> signal = new Signal<int, string>();
+        //Add a listener
+        signal.AddListener(callbackWithParamsIntAndString);
+        //Add a listener for the duration of precisely one Dispatch
+        signal.AddOnceListener(anotherCallbackWithParamsIntAndString);
+        //These all throw compile-time errors
+        signal.AddListener(callbackWithParamsStringAndInt);
+        signal.AddListener(callbackWithOneParameter);
+        signal.AddListener(callbackWithNoParameters);
+        //Dispatch
+        signal.Dispatch(42, "zaphod");
+        //Remove the first listener. The listener added by AddOnceListener has been automatically removed.
+        signal.RemoveListener(callbackWithParamsIntAndString);
+ *
  * @see strange.extensions.signal.api.IBaseSignal
  * @see strange.extensions.signal.impl.BaseSignal
  */
@@ -67,6 +67,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Cysharp.Threading.Tasks;
 
 namespace EDIVE.External.Signals
 {
@@ -82,12 +83,9 @@ namespace EDIVE.External.Signals
         public event Action Listener = null;
         public event Action OnceListener = null;
 
-        public void AddListener(Action callback)
-        {
-            Listener = this.AddUnique(Listener, callback);
-        }
+        public void AddListener(Action callback) { Listener = this.AddUnique(Listener, callback); }
 
-        public void AddOnce(Action callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+        public void AddOnceListener(Action callback) { OnceListener = this.AddUnique(OnceListener, callback); }
 
         public void RemoveListener(Action callback)
         {
@@ -99,6 +97,13 @@ namespace EDIVE.External.Signals
         {
             if (OnceListener != null)
                 OnceListener -= callback;
+        }
+
+        public async UniTask Await()
+        {
+            var tcs = new UniTaskCompletionSource();
+            AddOnceListener(() => tcs.TrySetResult());
+            await tcs.Task;
         }
 
         public override List<Type> GetTypes() { return new List<Type>(); }
@@ -139,7 +144,14 @@ namespace EDIVE.External.Signals
 
         public void AddListener(Action<T> callback) { Listener = this.AddUnique(Listener, callback); }
 
-        public void AddOnce(Action<T> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+        public void AddOnceListener(Action<T> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public async UniTask<T> Await()
+        {
+            var tcs = new UniTaskCompletionSource<T>();
+            AddOnceListener(r => tcs.TrySetResult(r));
+            return await tcs.Task;
+        }
 
         public void RemoveListener(Action<T> callback)
         {
@@ -197,7 +209,14 @@ namespace EDIVE.External.Signals
 
         public void AddListener(Action<T, U> callback) { Listener = this.AddUnique(Listener, callback); }
 
-        public void AddOnce(Action<T, U> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+        public void AddOnceListener(Action<T, U> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public async UniTask<(T, U)> Await()
+        {
+            var tcs = new UniTaskCompletionSource<(T, U)>();
+            AddOnceListener((t, u) => tcs.TrySetResult((t, u)));
+            return await tcs.Task;
+        }
 
         public void RemoveListener(Action<T, U> callback)
         {
@@ -256,7 +275,14 @@ namespace EDIVE.External.Signals
 
         public void AddListener(Action<T, U, V> callback) { Listener = this.AddUnique(Listener, callback); }
 
-        public void AddOnce(Action<T, U, V> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+        public void AddOnceListener(Action<T, U, V> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public async UniTask<(T, U, V)> Await()
+        {
+            var tcs = new UniTaskCompletionSource<(T, U, V)>();
+            AddOnceListener((t, u, v) => tcs.TrySetResult((t, u, v)));
+            return await tcs.Task;
+        }
 
         public void RemoveListener(Action<T, U, V> callback)
         {
@@ -313,7 +339,14 @@ namespace EDIVE.External.Signals
 
         public void AddListener(Action<T, U, V, W> callback) { Listener = this.AddUnique(Listener, callback); }
 
-        public void AddOnce(Action<T, U, V, W> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+        public void AddOnceListener(Action<T, U, V, W> callback) { OnceListener = this.AddUnique(OnceListener, callback); }
+
+        public async UniTask<(T, U, V, W)> Await()
+        {
+            var tcs = new UniTaskCompletionSource<(T, U, V, W)>();
+            AddOnceListener((t, u, v, w) => tcs.TrySetResult((t, u, v, w)));
+            return await tcs.Task;
+        }
 
         public void RemoveListener(Action<T, U, V, W> callback)
         {
