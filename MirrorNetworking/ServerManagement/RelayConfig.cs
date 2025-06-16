5b9900dadac10ee36aa3017f274acff9fc2012d0
František Holubec <frantisek.holubec@gmail.com>
2025-06-16 07:05:30 +0200

    Relay init wip

---
+++ MirrorNetworking/ServerManagement/RelayConfig.cs
@@ -0,0 +1,69 @@
+﻿// Author: František Holubec
+// Created: 15.06.2025
+
+using Newtonsoft.Json;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace EDIVE.MirrorNetworking.ServerManagement
+{
+    [JsonObject(MemberSerialization.OptIn)]
+    public class RelayConfig : ScriptableObject
+    {
+        [JsonProperty]
+        [SerializeField]
+        private string _NodeIP;
+
+        [JsonProperty]
+        [SerializeField]
+        private string _NodePort;
+
+        [JsonProperty]
+        [SerializeField]
+        private string _EndpointPort;
+
+        [JsonProperty]
+        [SerializeField]
+        private string _AuthKey;
+
+        [JsonProperty]
+        [SerializeField]
+        [Range(0.1f, 5f)]
+        private float _HeartbeatInterval = 3f;
+
+        [JsonProperty]
+        [SerializeField]
+        private bool _UseNATPunch;
+
+        [JsonProperty]
+        [SerializeField]
+        private bool _UseLoadBalancer;
+
+        [JsonProperty]
+        [SerializeField]
+        [EnableIf(nameof(_UseLoadBalancer))]
+        private string _LoadBalancerIP;
+
+        [JsonProperty]
+        [SerializeField]
+        [EnableIf(nameof(_UseLoadBalancer))]
+        private string _LoadBalancerPort;
+
+        [JsonProperty]
+        [SerializeField]
+        private int _AppID = 1;
+
+        [JsonProperty]
+        [SerializeField]
+        private string _ServerName;
+
+        [JsonProperty]
+        [SerializeField]
+        private int _MaxPlayers = 10;
+
+        [JsonProperty]
+        [SerializeField]
+        private bool _IsPublic = true;
+
+    }
+}
---
+++ MirrorNetworking/ServerManagement/RelayInitializer.cs
@@ -0,0 +1,25 @@
+﻿// Author: František Holubec
+// Created: 15.06.2025
+
+using System;
+using Cysharp.Threading.Tasks;
+using EDIVE.AppLoading.Loadables;
+using LightReflectiveMirror;
+using UnityEngine;
+
+namespace EDIVE.MirrorNetworking.ServerManagement
+{
+    [RequireComponent(typeof(LightReflectiveMirrorTransport))]
+    public class RelayInitializer : MonoBehaviour, ILoadable
+    {
+        [SerializeField]
+        private RelayConfig _Config;
+
+        public UniTask Load(Action<float> progressCallback)
+        {
+            var transport = GetComponent<LightReflectiveMirrorTransport>();
+            transport.ConnectToRelay();
+            return UniTask.CompletedTask;
+        }
+    }
+}
