43aab5983097570dc9cfc7e96e0ff5033444133a
František Holubec <frantisek.holubec@gmail.com>
2025-06-23 09:47:51 +0200

    Tag selector cleanup

--- OdinExtensions/Attributes/TagSelectorAttribute.cs
+++ OdinExtensions/Attributes/TagSelectorAttribute.cs
@@ -1,11 +1,6 @@
-// Author: František Holubec
-// Created: 06.03.2022
-// Copyright (c) Noxgames
-// http://www.noxgames.com/
-
 using System;
 
-namespace ProtoGIS.Scripts.Utils.OdinExtensions
+namespace EDIVE.OdinExtensions.Attributes
 {
     public class TagSelectorAttribute : Attribute
     {
--- OdinExtensions/Editor/Drawers/TagSelectorAttributeDrawer.cs
+++ OdinExtensions/Editor/Drawers/TagSelectorAttributeDrawer.cs
@@ -1,13 +1,9 @@
-// Author: František Holubec
-// Created: 06.03.2022
-// Copyright (c) Noxgames
-// http://www.noxgames.com/
-
+using EDIVE.OdinExtensions.Attributes;
 using Sirenix.OdinInspector.Editor;
 using UnityEditor;
 using UnityEngine;
 
-namespace ProtoGIS.Scripts.Utils.OdinExtensions.Editor
+namespace EDIVE.OdinExtensions.Editor.Drawers
 {
     public class TagSelectorAttributeDrawer : OdinAttributeDrawer<TagSelectorAttribute, string>
     {
