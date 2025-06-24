f3668df78ca3e4b0935736a4efe25ad93bbdd229
Radim Holub <radikholub1@seznam.cz>
2025-06-24 07:39:47 +0200

    Transformer-based attach manipulation

---
+++ XRTools/Interactions/AInteractorTransformer.cs
@@ -0,0 +1,14 @@
+﻿// Author: Radim Holub
+// Created: 18.06.2025
+
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace EDIVE.XRTools.Interactions
+{
+    public abstract class AInteractorTransformer : MonoBehaviour
+    {
+        public abstract void PreprocessInteractor(XRInteractionUpdateOrder.UpdatePhase updatePhase);
+        public abstract bool IsManipulating();
+    }
+}
