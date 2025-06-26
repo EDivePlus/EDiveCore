c4a31b12fd9eed4a708ee638f9e03d5c0e53bd3b
František Holubec <frantisek.holubec@gmail.com>
2025-06-26 12:56:35 +0200

    Tablet controller and manager

---
+++ XRTools/Tablet/TabletController.cs
@@ -0,0 +1,28 @@
+﻿// Author: František Holubec
+// Created: 26.06.2025
+
+using EDIVE.XRTools.Keyboard;
+using UnityEngine;
+
+namespace EDIVE.XRTools.Tablet
+{
+    public class TabletController : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyboardController _Keyboard;
+
+        [SerializeField]
+        private Canvas _MainLayer;
+
+        [SerializeField]
+        private Canvas _OverlayLayer;
+
+        [SerializeField]
+        private Canvas _DebugLayer;
+
+        public KeyboardController Keyboard => _Keyboard;
+        public Canvas MainLayer => _MainLayer;
+        public Canvas OverlayLayer => _OverlayLayer;
+        public Canvas DebugLayer => _DebugLayer;
+    }
+}
---
+++ XRTools/Tablet/TabletManager.cs
@@ -0,0 +1,66 @@
+﻿// Author: František Holubec
+// Created: 26.06.2025
+
+using EDIVE.Core.Services;
+using EDIVE.DataStructures.ScriptableVariables.Variables;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit.Utilities;
+
+namespace EDIVE.XRTools.Tablet
+{
+    public class TabletManager : AServiceBehaviour<TabletManager>
+    {
+        [SerializeField]
+        private TabletController _Tablet;
+
+        [SerializeField]
+        private TransformScriptableVariable _CameraTransformVariable;
+
+        [SerializeField]
+        private InputActionReference _RepositionAction;
+
+        [SerializeField]
+        private Vector3 _PositionOffset;
+
+        private Transform CameraTransform => _CameraTransformVariable != null && _CameraTransformVariable.Value != null
+                ? _CameraTransformVariable.Value
+                : Camera.main?.transform;
+
+        protected override void Awake()
+        {
+            base.Awake();
+            if(_RepositionAction)
+                _RepositionAction.action.performed += OnRepositionPerformed;
+        }
+
+        private void OnRepositionPerformed(InputAction.CallbackContext context)
+        {
+            RepositionTablet();
+        }
+
+        [Button]
+        public void RepositionTablet()
+        {
+            var target = CameraTransform;
+            if (target == null)
+                return;
+
+            var position = target.position +
+                           target.right * _PositionOffset.x +
+                           target.forward * _PositionOffset.z +
+                           Vector3.up * _PositionOffset.y;
+            _Tablet.transform.position = position;
+            _Tablet.transform.localScale = Vector3.one;
+            FaceKeyboardAtTarget(target);
+        }
+
+        private void FaceKeyboardAtTarget(Transform target)
+        {
+            var forward = (_Tablet.transform.position - target.position).normalized;
+            BurstMathUtility.OrthogonalLookRotation(forward, Vector3.up, out var newTarget);
+            _Tablet.transform.rotation = newTarget;
+        }
+    }
+}
