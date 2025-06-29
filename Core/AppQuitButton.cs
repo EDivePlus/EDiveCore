bb3a8fc73fbe6920ca94016807fcf6ad16bc0ba9
František Holubec <frantisek.holubec@gmail.com>
2025-06-29 18:25:31 +0200

    Restart and quit buttons

---
+++ Core/AppQuitButton.cs
@@ -0,0 +1,35 @@
+﻿// Author: František Holubec
+// Created: 29.06.2025
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EDIVE.Core
+{
+    [RequireComponent(typeof(Button))]
+    public class AppQuitButton : MonoBehaviour
+    {
+        private Button _button;
+        
+        private void Awake()
+        {
+            if (TryGetComponent(out _button))
+                _button.onClick.AddListener(OnButtonClicked);
+        }
+        
+        private void OnDestroy()
+        {
+            if (_button != null) 
+                _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
+    }
+}
---
+++ Core/Restart/AppRestartButton.cs
@@ -0,0 +1,31 @@
+﻿// Author: František Holubec
+// Created: 29.06.2025
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EDIVE.Core.Restart
+{
+    [RequireComponent(typeof(Button))]
+    public class AppRestartButton : MonoBehaviour
+    {
+        private Button _button;
+        
+        private void Awake()
+        {
+            if (TryGetComponent(out _button))
+                _button.onClick.AddListener(OnButtonClicked);
+        }
+        
+        private void OnDestroy()
+        {
+            if (_button != null) 
+                _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            AppRestartUtility.Restart();
+        }
+    }
+}
