2123cf643e96a01c85fdc3efaa3f93703019ce57
František Holubec <frantisek.holubec@gmail.com>
2025-07-03 18:21:27 +0200

    Child GO only validator

---
+++ OdinExtensions/Attributes/EnhancedChildGameObjectsOnlyAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Author: František Holubec
+// Created: 03.07.2025
+
+using System;
+using System.Diagnostics;
+
+namespace EDIVE.OdinExtensions.Attributes
+{
+    [Conditional("UNITY_EDITOR")]
+    public class EnhancedChildGameObjectsOnlyAttribute : Attribute
+    {
+        public bool IncludeSelf = true;
+
+        public EnhancedChildGameObjectsOnlyAttribute() { }
+        public EnhancedChildGameObjectsOnlyAttribute(bool includeSelf)
+        {
+            IncludeSelf = includeSelf;
+        }
+    }
+}
---
+++ OdinExtensions/Editor/Validators/EnhancedChildGameObjectsOnlyValidator.cs
@@ -0,0 +1,57 @@
+﻿// Author: František Holubec
+// Created: 03.07.2025
+
+using EDIVE.OdinExtensions.Attributes;
+using EDIVE.OdinExtensions.Editor.Validators;
+using Sirenix.OdinInspector.Editor.Validation;
+using UnityEngine;
+
+[assembly: RegisterValidator(typeof(EnhancedChildGameObjectsOnlyValidator<>))]
+namespace EDIVE.OdinExtensions.Editor.Validators
+{
+    public class EnhancedChildGameObjectsOnlyValidator<T> : AttributeValidator<EnhancedChildGameObjectsOnlyAttribute, T> where T : Object
+    {
+        protected override void Validate(ValidationResult result)
+        {
+            var ownerGo = result.Setup.Root as GameObject;
+            if (ownerGo == null)
+            {
+                var component = result.Setup.Root as Component;
+                if (component != null)
+                    ownerGo = component.gameObject;
+            }
+
+            var valueGo = ValueEntry.SmartValue as GameObject;
+            if (valueGo == null)
+            {
+                var component = ValueEntry.SmartValue as Component;
+
+                if (component != null)
+                {
+                    valueGo = component.gameObject;
+                }
+            }
+
+            // Attribute doesn't apply in this context, as we're not on a GameObject
+            // or are not dealing with the right kind of value
+            if (ownerGo == null || valueGo == null)
+                return;
+
+            if (Attribute.IncludeSelf && ownerGo == valueGo)
+                return;
+
+            var current = valueGo.transform;
+            while (true)
+            {
+                current = current.parent;
+                if (current == null)
+                    break;
+
+                if (current.gameObject == ownerGo)
+                    return;
+            }
+
+            result.AddError($"{valueGo.name} must be a child of {ownerGo.name}");
+        }
+    }
+}
