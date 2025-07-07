7b5d5ba54dbb9a68a7f6ea7f8a7cc877dbadd12c
František Holubec <frantisek.holubec@gmail.com>
2025-07-07 09:48:27 +0200

    Memory pack utils

---
+++ Utils/MemoryPack/MemoryPackUnionTagAttribute.cs
@@ -0,0 +1,18 @@
+﻿// Author: František Holubec
+// Created: 07.07.2025
+
+using System;
+
+namespace EDIVE.Utils.MemoryPack
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
+    public class MemoryPackUnionTagAttribute : Attribute
+    {
+        public ushort Tag { get; }
+
+        public MemoryPackUnionTagAttribute(ushort tag)
+        {
+            Tag = tag;
+        }
+    }
+}
---
+++ Utils/MemoryPack/MemoryPackUtility.cs
@@ -0,0 +1,54 @@
+﻿// Author: František Holubec
+// Created: 07.07.2025
+
+#if MEMORY_PACK
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using EDIVE.NativeUtils;
+using MemoryPack;
+using MemoryPack.Formatters;
+using UnityEngine;
+
+namespace EDIVE.Utils.MemoryPack
+{
+    public static class MemoryPackUtility
+    {
+        public static void RegisterDynamicUnionFormatter<T>() where T : class
+        {
+            var taggedTypes = new List<(ushort Tag, Type Type)>();
+            foreach (var type in typeof(T).GetAssignableTypes())
+            {
+                var tagAttr = type.GetCustomAttribute<MemoryPackUnionTagAttribute>();
+                if (tagAttr == null)
+                {
+#if UNITY_EDITOR
+                    Debug.LogWarning($"[MemoryPack] Skipping {type.FullName} — missing [MemoryPackUnionTag] attribute.");
+#endif
+                    continue;
+                }
+                taggedTypes.Add((tagAttr.Tag, type));
+            }
+
+#if UNITY_EDITOR
+            var duplicateTags = taggedTypes
+                .GroupBy(t => t.Tag)
+                .Where(g => g.Count() > 1)
+                .ToList();
+
+            if (duplicateTags.Any())
+            {
+                foreach (var group in duplicateTags)
+                {
+                    Debug.LogError($"[MemoryPack] Duplicate tag {group.Key} used by: {string.Join(", ", group.Select(t => t.Type.Name))}");
+                }
+            }
+#endif
+
+            var formatter = new DynamicUnionFormatter<T>(taggedTypes.ToArray());
+            MemoryPackFormatterProvider.Register(formatter);
+        }
+    }
+}
+#endif
