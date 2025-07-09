3c14a76555f205d8cd7ddd59a6e460d08f2bf399
František Holubec <frantisek.holubec@gmail.com>
2025-07-09 12:18:38 +0200

    Tablet toggle button

---
+++ XRTools/Tablet/TabletToggleButton.cs
@@ -0,0 +1,35 @@
+﻿// Author: František Holubec
+// Created: 09.07.2025
+
+using EDIVE.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EDIVE.XRTools.Tablet
+{
+    [RequireComponent(typeof(Button))]
+    public class TabletToggleButton : MonoBehaviour
+    {
+        private Button _button;
+
+        private void Awake()
+        {
+            if (TryGetComponent(out _button))
+                _button.onClick.AddListener(OnButtonClicked);
+        }
+
+        private void OnDestroy()
+        {
+            if (_button != null)
+                _button.onClick.RemoveListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            if (AppCore.Services.TryGet<TabletController>(out var tablet))
+            {
+                tablet.ToggleTablet();
+            }
+        }
+    }
+}
