ad53b88f3ddfece0c00bf79f93ce2d7cedd1d410
František Holubec <frantisek.holubec@gmail.com>
2025-07-21 16:47:45 +0200

    AssetTranslation memory pack utils

---
+++ AssetTranslation/MemoryPack/AssetTranslationMemoryPackUtils.cs
@@ -0,0 +1,21 @@
+﻿// Author: František Holubec
+// Created: 21.07.2025
+
+#if MEMORY_PACK
+using MemoryPack;
+using MemoryPack.Formatters;
+using UnityEngine;
+
+namespace EDIVE.AssetTranslation
+{
+    public static class AssetTranslationMemoryPackUtils
+    {
+        public static void RegisterTranslator<TDefinition>() where TDefinition : ScriptableObject, IUniqueDefinition
+        {
+            MemoryPackFormatterProvider.Register(new AssetTranslationMemoryPackFormatter<TDefinition>());
+            MemoryPackFormatterProvider.Register(new ArrayFormatter<TDefinition>());
+            MemoryPackFormatterProvider.Register(new ListFormatter<TDefinition>());
+        }
+    }
+}
+#endif
