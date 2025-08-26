a83d27e3a8f6071c6940ef90f0a42b8e252a0571
František Holubec <frantisek.holubec@gmail.com>
2025-08-26 15:15:10 +0200

    Lighting wip

---
+++ Lighting/LightingManager.cs
@@ -0,0 +1,50 @@
+﻿// Author: František Holubec
+// Created: 26.08.2025
+
+using EDIVE.Core.Services;
+using EDIVE.OdinExtensions.Attributes;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace EDIVE.Lighting
+{
+    public class LightingManager : AServiceBehaviour<LightingManager>
+    {
+        [ShowCreateNew]
+        [SerializeField]
+        private SceneLightingConfig _DefaultLightingConfig;
+        
+        private static readonly int MAIN_TEX = Shader.PropertyToID("_MainTex");
+
+        protected override void Awake()
+        {
+            base.Awake();
+            SetLighting(_DefaultLightingConfig);
+        }
+
+        [Button]
+        public void SetLighting(SceneLightingConfig lightingConfig)
+        {
+            if (lightingConfig == null)
+                return;
+            
+            var sunLight = RenderSettings.sun;
+            if (sunLight != null)
+            {
+                sunLight.enabled = lightingConfig.EnableSun;
+                sunLight.transform.rotation = Quaternion.Euler(lightingConfig.SunDirection);
+                sunLight.color = lightingConfig.SunColor;
+                sunLight.intensity = lightingConfig.SunIntensity;
+                sunLight.bounceIntensity = lightingConfig.SunIndirectMultiplier;
+                DynamicGI.UpdateEnvironment();
+            }
+            
+            var skybox = RenderSettings.skybox;
+            if (skybox != null)
+            {
+                if (lightingConfig.SkyboxTexture != null)
+                    skybox.SetTexture(MAIN_TEX, lightingConfig.SkyboxTexture);
+            }
+        }
+    }
+}
---
+++ Lighting/SceneLightingConfig.cs
@@ -0,0 +1,35 @@
+﻿// Author: František Holubec
+// Created: 26.08.2025
+
+using UnityEngine;
+
+namespace EDIVE.Lighting
+{
+    public class SceneLightingConfig : ScriptableObject
+    {
+        [SerializeField]
+        private Texture2D _SkyboxTexture;
+        
+        [SerializeField]
+        private Vector3 _SunDirection = new(50, -30, 0);
+        
+        [SerializeField]
+        private Color _SunColor = new(1,0.92f, 0.83f);
+        
+        [SerializeField]
+        private bool _EnableSun = true;
+        
+        [SerializeField]
+        private float _SunIntensity = 1;
+        
+        [SerializeField]
+        private float _SunIndirectMultiplier = 1;
+
+        public Texture2D SkyboxTexture => _SkyboxTexture;
+        public Vector3 SunDirection => _SunDirection;
+        public Color SunColor => _SunColor;
+        public float SunIntensity => _SunIntensity;
+        public float SunIndirectMultiplier => _SunIndirectMultiplier;
+        public bool EnableSun => _EnableSun;
+    }
+}
