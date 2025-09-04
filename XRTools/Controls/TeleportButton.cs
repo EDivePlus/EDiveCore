ec78bcd0476308551ac0d9aa9d281fd87add73f5
František Holubec <frantisek.holubec@gmail.com>
2025-09-04 09:31:03 +0200

    Teleport Button

---
+++ XRTools/Controls/TeleportButton.cs
@@ -0,0 +1,41 @@
+﻿// Author: František Holubec
+// Created: 03.09.2025
+
+using EDIVE.Core;
+using EDIVE.ScriptableArchitecture.Variables;
+using EDIVE.Utils.Activations;
+using UnityEngine;
+
+namespace EDIVE.XRTools.Controls
+{
+    public class TeleportButton : MonoBehaviour
+    {
+        [SerializeReference]
+        private IActivation _Activation;
+        
+        [SerializeField]
+        private ScriptableVariableField<Transform> _Location;
+        
+        private void Awake()
+        {
+            _Activation?.RegisterActivationListener(OnActivated);
+        }
+        
+        private void OnDestroy()
+        {
+            _Activation?.UnregisterActivationListener(OnActivated);
+        }
+
+        private void OnActivated()
+        {
+            var location = _Location.Value;
+            if (location == null)
+                return;
+            
+            if (AppCore.Services.TryGet<ControlsManager>(out var controlsManager))
+            {
+                controlsManager.RequestTeleport(location.position, location.rotation);
+            }
+        }
+    }
+}
