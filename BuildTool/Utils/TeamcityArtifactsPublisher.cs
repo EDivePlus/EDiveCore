c8c60bdb7c886ff851807062f6c54f78d5a5bfba
František Holubec <frantisek.holubec@gmail.com>
2025-09-26 10:47:00 +0200

    Teamcity publisher - split zip and publish

---
+++ BuildTool/Utils/TeamcityArtifactsPublisher.cs
@@ -0,0 +1,46 @@
+﻿// Author: František Holubec
+// Created: 25.09.2025
+
+using System;
+using System.IO;
+using EDIVE.NativeUtils.TeamCity;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using File = UnityEngine.Windows.File;
+
+namespace EDIVE.BuildTool.Utils
+{
+    [Serializable]
+    public class TeamcityArtifactsPublisher : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 10000;
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (report.summary.result != BuildResult.Succeeded)
+                return;
+
+            if (!TryGetArtifactPath(report, out var artifactPath))
+            {
+                Debug.LogWarning("[BuildTool] No artifacts to publish.");
+                return;
+            }
+
+            Debug.Log($"[BuildTool] Publishing artifact: {artifactPath}");
+            TeamCityServiceMessages.PublishArtifacts(artifactPath);
+        }
+
+        private bool TryGetArtifactPath(BuildReport report, out string path)
+        {
+            path = report.summary.platform switch
+            {
+                BuildTarget.Android => report.summary.outputPath,
+                BuildTarget.StandaloneWindows or BuildTarget.StandaloneWindows64 or BuildTarget.StandaloneLinux64 => Path.ChangeExtension(report.summary.outputPath, ".zip"),
+                _ => null
+            };
+            return !string.IsNullOrEmpty(path) && File.Exists(path);
+        }
+    }
+}
---
+++ BuildTool/Utils/ZipResultBuildProcessor.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using UnityEditor;
+using UnityEditor.Build;
+using UnityEditor.Build.Reporting;
+using UnityEngine;
+using CompressionLevel = System.IO.Compression.CompressionLevel;
+
+namespace EDIVE.BuildTool.Utils
+{
+    public class ZipResultBuildProcessor : IPostprocessBuildWithReport
+    {
+        public int callbackOrder => 9999;
+        
+        private static readonly string[] DONT_INCLUDE =
+        {
+            "_DoNotShip",
+            "_ButDontShipItWithYourGame"
+        };
+
+        public void OnPostprocessBuild(BuildReport report)
+        {
+            if (report.summary.result != BuildResult.Succeeded)
+                return;
+
+            if (!ShouldZip(report))
+                return;
+
+            Debug.Log("[BuildTool] Zipping build...");
+            var summary = report.summary;
+            var files = report.GetFiles();
+
+            var buildFolderPath = Path.GetDirectoryName(summary.outputPath);
+            var outputZipPath = Path.ChangeExtension(summary.outputPath, ".zip");
+
+            if (outputZipPath == null || buildFolderPath == null)
+            {
+                Debug.LogError("[BuildTool] Zipping build failed! Output path is null.");
+                return;
+            }
+
+            if (File.Exists(outputZipPath))
+                File.Delete(outputZipPath);
+
+            try
+            {
+                using var zip = ZipFile.Open(outputZipPath, ZipArchiveMode.Create);
+                foreach (var file in files)
+                {
+                    var filePath = file.path;
+                    var fileName = Path.GetFileName(filePath);
+                
+                    if (!CheckIncludePath(fileName))
+                        continue;
+                
+                    var relativePath = Path.GetRelativePath(buildFolderPath, filePath);
+                    zip.CreateEntryFromFile(EnsureValidPath(filePath), relativePath, CompressionLevel.Optimal);
+                }
+                Debug.Log($"[BuildTool] Zipped build to {outputZipPath}");
+            }
+            catch (Exception e)
+            {
+                Debug.LogException(e);
+                Debug.LogError($"[BuildTool] Zipping build to {outputZipPath} failed with exception.");
+            }
+        }
+
+        private static bool ShouldZip(BuildReport report)
+        {
+            return report.summary.platform is BuildTarget.StandaloneWindows or BuildTarget.StandaloneWindows64 or BuildTarget.StandaloneLinux64;
+        }
+
+        private static string EnsureValidPath(string path)
+        {
+            if (string.IsNullOrEmpty(path))
+                return path;
+            
+            var fullPath = Path.GetFullPath(path);
+            
+            // Windows MAX_PATH limitation workaround
+            if (fullPath.Length >= 260 && !fullPath.StartsWith(@"\\?\"))
+                return @"\\?\" + fullPath;
+
+            return fullPath;
+        }
+        
+        private static bool CheckIncludePath(string path)
+        {
+            return DONT_INCLUDE.All(exclude => !path.Contains(exclude));
+        }
+    }
+}
