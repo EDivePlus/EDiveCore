4dead44d6216c48cdd978158d537fc49737fb1e2
František Holubec <frantisek.holubec@gmail.com>
2025-10-13 08:06:30 +0200

    Light observer updater

---
+++ Networking/Utils/LightsObserverUpdater.cs
@@ -0,0 +1,30 @@
+﻿// Author: Radim Holub
+// Created: 10.10.2025
+using System.Collections.Generic;
+using System.Linq;
+using FishNet.Object;
+using UnityEngine;
+
+namespace _Projects.PlayGround.Scripts
+{
+    public class LightsObserverUpdater : NetworkBehaviour
+    {
+        private List<Light> _lights = new();
+        
+        private void Awake()
+        {
+            _lights = GetComponentsInChildren<Light>().ToList();
+            NetworkObject.OnHostVisibilityUpdated += OnHostVisibilityUpdated;
+        }
+        
+        private void OnDestroy()
+        {
+            NetworkObject.OnHostVisibilityUpdated -= OnHostVisibilityUpdated;
+        }
+
+        private void OnHostVisibilityUpdated(bool prevVisible, bool nextVisible)
+        {
+            _lights.ForEach(l => l.enabled = nextVisible);
+        }
+    }
+}
