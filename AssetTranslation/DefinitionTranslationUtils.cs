b13139424af1df882ad264ffb061dfe9fb3f8c4c
František Holubec <frantisek.holubec@gmail.com>
2025-10-14 08:47:52 +0200

    Asset translation utility for Fishnet

--- AssetTranslation/DefinitionTranslationUtils.cs
+++ AssetTranslation/DefinitionTranslationUtils.cs
@@ -1,10 +1,14 @@
 ﻿// Author: František Holubec
 // Created: 14.04.2025
 
+
 using UnityEngine;
 
 #if MIRROR
 using Mirror;
+#endif
+#if FISHNET
+using FishNet.Serializing;
 #endif
 
 namespace EDIVE.AssetTranslation
@@ -31,6 +35,38 @@ namespace EDIVE.AssetTranslation
         {
             return TryGetDefinition<TDefinition>(uniqueId, out var resultDefinition) ? resultDefinition : null;
         }
+        
+#if FISHNET
+        public static void CustomWriteDefinition<TDefinition>(this Writer writer, TDefinition value) where TDefinition : ScriptableObject, IUniqueDefinition
+        {
+#if UNITY_EDITOR || ASSET_TRANSLATION_LOGS
+            if (AssetTranslationConfig.Instance.TryGetTranslator(value.GetType(), out var translator))
+            {
+                if (value is ScriptableObject so && !translator.Contains(so))
+                {
+                    Debug.LogError($"Definition with id '{value.UniqueID}' is not registered in translator '{translator.name}'");
+                }
+            }
+            else
+            {
+                Debug.LogError($"There is no translator for type '{value.GetType()}' or non is registered");
+            }
+#endif
+            writer.WriteString(value != null ? value.UniqueID : string.Empty);
+        }
+
+        public static TDefinition CustomReadDefinition<TDefinition>(this Reader reader) where TDefinition : ScriptableObject, IUniqueDefinition
+        {
+            var uniqueId = reader.ReadStringAllocated();
+            if (string.IsNullOrEmpty(uniqueId))
+                return null;
+
+            if (!AssetTranslationConfig.Instance.TryGetTranslator(typeof(TDefinition), out var translator))
+                return null;
+
+            return translator.TryGet(uniqueId, out var definition) && definition is TDefinition tDefinition ? tDefinition : null;
+        }
+#endif
 
 #if MIRROR
         public static void WriteDefinition<TDefinition>(this NetworkWriter writer, TDefinition value) where TDefinition : ScriptableObject, IUniqueDefinition
