c93c15e7850646b9ad2effbcda81106592804b69
František Holubec <frantisek.holubec@gmail.com>
2025-10-20 10:24:32 +0200

    GUI background color attribute

---
+++ OdinExtensions/Attributes/GUIBackgroundColorAttribute.cs
@@ -0,0 +1,20 @@
+﻿// Author: František Holubec
+// Created: 20.10.2025
+
+using System;
+using System.Diagnostics;
+using UnityEngine;
+
+namespace EDIVE.OdinExtensions.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    [Conditional("UNITY_EDITOR")]
+    public class GUIBackgroundColorAttribute : Attribute
+    {
+        public Color Color;
+        public string GetColor;
+        
+        public GUIBackgroundColorAttribute(float r, float g, float b, float a = 1f) => Color = new Color(r, g, b, a);
+        public GUIBackgroundColorAttribute(string getColor) => GetColor = getColor;
+    }
+}
---
+++ OdinExtensions/Editor/Drawers/GUIBackgroundColorAttributeDrawer.cs
@@ -0,0 +1,36 @@
+﻿// Author: František Holubec
+// Created: 20.10.2025
+
+using EDIVE.OdinExtensions.Attributes;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.OdinInspector.Editor.ValueResolvers;
+using UnityEngine;
+
+namespace EDIVE.OdinExtensions.Editor.Drawers
+{
+    [DrawerPriority(0.5)]
+    public sealed class GUIBackgroundColorAttributeDrawer : OdinAttributeDrawer<GUIBackgroundColorAttribute>
+    {
+        private ValueResolver<Color> _colorResolver;
+
+        protected override void Initialize()
+        {
+            _colorResolver = ValueResolver.Get(Property, Attribute.GetColor, Attribute.Color);
+        }
+        
+        protected override void DrawPropertyLayout(GUIContent label)
+        {
+            ValueResolver.DrawErrors(_colorResolver);
+            if (_colorResolver.HasError)
+            {
+                CallNextDrawer(label);
+                return;
+            }
+
+            var previousColor = GUI.backgroundColor;
+            GUI.backgroundColor = _colorResolver.GetValue();
+            CallNextDrawer(label);
+            GUI.backgroundColor = previousColor;
+        }
+    }
+}
