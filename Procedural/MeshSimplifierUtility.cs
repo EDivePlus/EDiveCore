8845e806c90a03034399ed2fded1de80d9c6dd87
František Holubec <frantisek.holubec@gmail.com>
2025-10-29 12:56:02 +0100

    Compilation fix mesh simplifier

--- Procedural/MeshSimplifierUtility.cs
+++ Procedural/MeshSimplifierUtility.cs
@@ -2,16 +2,18 @@
 // Created: 19.10.2021
 
 #if MESH_SIMPLIFIER
-
 using System;
 using System.Collections.Generic;
 using System.IO;
 using Sirenix.OdinInspector;
-using UnityEditor;
-using UnityEditor.Formats.Fbx.Exporter;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityMeshSimplifier;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using UnityEditor.Formats.Fbx.Exporter;
+#endif
 
 namespace EDIVE.Procedural
 {
