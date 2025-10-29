ef2ab767f30d2a857117cfca84148fc863c81ef0
František Holubec <frantisek.holubec@gmail.com>
2025-10-29 11:23:31 +0100

    Interaction manager assigner - execute before xr components

--- XRTools/Controls/InteractionManagerAssigner.cs
+++ XRTools/Controls/InteractionManagerAssigner.cs
@@ -8,6 +8,7 @@ using UnityEngine.XR.Interaction.Toolkit.Locomotion.Teleportation;
 
 namespace EDIVE.XRTools.Controls
 {
+    [DefaultExecutionOrder(-100)]
     [RequireComponent(typeof(XRBaseInteractable))]
     public class InteractionManagerAssigner : MonoBehaviour
     {
