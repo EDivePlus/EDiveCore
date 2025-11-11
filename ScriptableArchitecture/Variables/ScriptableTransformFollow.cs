2265a429013d98550980f48ef260331caa62dcad
František Holubec <frantisek.holubec@gmail.com>
2025-11-11 10:12:28 +0100

    scriptable follow refactor wip

--- ScriptableArchitecture/Variables/ScriptableTransformFollow.cs
+++ ScriptableArchitecture/Variables/ScriptableTransformFollow.cs
@@ -1,6 +1,7 @@
 ﻿// Author: František Holubec
 // Created: 12.06.2025
 
+using EDIVE.NativeUtils;
 using EDIVE.ScriptableArchitecture.Variables.Impl;
 using UnityEngine;
 
@@ -16,7 +17,7 @@ namespace EDIVE.ScriptableArchitecture.Variables
             get => _Source;
             set => _Source = value;
         }
-
+        
         private void LateUpdate()
         {
             if (_Source?.Value)
@@ -25,5 +26,42 @@ namespace EDIVE.ScriptableArchitecture.Variables
                 transform.rotation = _Source.Value.rotation;
             }
         }
+      
+        // This does not work for some reason, i guess networking, or execution order...
+        /*
+        private Transform _currentTarget;
+
+        private void OnEnable()
+        {
+            if (_Source != null)
+            {
+                _Source.ValueChanged.AddListener(OnSourceValueChanged);
+                OnSourceValueChanged();
+            }
+        }
+
+        private void OnDisable()
+        {
+            if (_Source != null) 
+                _Source.ValueChanged.RemoveListener(OnSourceValueChanged);
+        }
+
+        private void OnSourceValueChanged()
+        {
+            if (_currentTarget != null) _currentTarget.RemoveChangeListener(OnTransformChanged);
+            _currentTarget = _Source.Value;
+            if (_currentTarget != null)
+            {
+                _currentTarget.AddChangeListener(OnTransformChanged);
+                OnTransformChanged(_currentTarget);
+            }
+        }
+
+        private void OnTransformChanged(Transform tr)
+        {
+            transform.position = _currentTarget.position;
+            transform.rotation = _currentTarget.rotation;
+        }
+        */
     }
 }
