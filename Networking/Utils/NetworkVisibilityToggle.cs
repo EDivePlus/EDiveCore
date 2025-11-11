06c8b41abadd542775a84e553014039a20d6d373
František Holubec <frantisek.holubec@gmail.com>
2025-11-11 08:57:21 +0100

    Network visibility toggle

---
+++ Networking/Utils/NetworkVisibilityToggle.cs
@@ -0,0 +1,36 @@
+﻿// Author: František Holubec
+// Created: 11.11.2025
+
+using EDIVE.StateHandling.ToggleStates;
+using FishNet.Object;
+using UnityEngine;
+
+namespace EDIVE.Networking.Utils
+{
+    public class NetworkVisibilityToggle : NetworkBehaviour
+    {
+        [SerializeField]
+        private AToggleState _ToggleState;
+        
+        private bool _initialVisibilitySet;
+
+        public override void OnStartNetwork()
+        {
+            base.OnStartNetwork();
+            _initialVisibilitySet = false;
+            NetworkObject.OnHostVisibilityUpdated += OnHostVisibilityUpdated;
+        }
+        
+        public override void OnStopNetwork()
+        {
+            base.OnStopNetwork();
+            NetworkObject.OnHostVisibilityUpdated -= OnHostVisibilityUpdated;
+        }
+        
+        private void OnHostVisibilityUpdated(bool prevVisible, bool nextVisible)
+        {
+            _ToggleState.SetState(nextVisible, !_initialVisibilitySet);
+            _initialVisibilitySet = true;
+        }
+    }
+}
