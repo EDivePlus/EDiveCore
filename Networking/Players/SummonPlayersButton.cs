7615895a55f078d901745dbfad1707c3d7925f4f
František Holubec <frantisek.holubec@gmail.com>
2026-01-22 11:07:31 +0100

    summon player fix

---
+++ Networking/Players/SummonPlayersButton.cs
@@ -0,0 +1,33 @@
+﻿// Author: František Holubec
+// Created: 22.01.2026
+
+using EDIVE.Core;
+using EDIVE.Utils.Activations;
+using UnityEngine;
+
+namespace EDIVE.Networking.Players
+{
+    public class SummonPlayersButton : MonoBehaviour
+    {
+        [SerializeReference]
+        private IActivation _Activation;
+        
+        private void Awake()
+        {
+            _Activation?.RegisterActivationListener(OnActivated);
+        }
+        
+        private void OnDestroy()
+        {
+            _Activation?.UnregisterActivationListener(OnActivated);
+        }
+
+        private void OnActivated()
+        {
+            if (AppCore.Services.TryGet<NetworkPlayerManager>(out var playerManager))
+            {
+                playerManager.Btn_SummonPlayersInMySceneToMe();
+            }
+        }
+    }
+}
