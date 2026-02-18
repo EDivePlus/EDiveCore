27fa6fe9880ad0e6d771dc2b51a63a25f3dbc9b4
František Holubec <frantisek.holubec@gmail.com>
2026-02-18 16:32:23 +0100

    Compound activation

---
+++ Utils/Activations/CompoundActivation.cs
@@ -0,0 +1,28 @@
+﻿// Author: František Holubec
+// Created: 18.02.2026
+
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EDIVE.Utils.Activations
+{
+    [Serializable]
+    public class CompoundActivation : IActivation
+    {
+        [SerializeReference]
+        private List<IActivation> _Activations = new();
+
+        public void RegisterActivationListener(Action onActivate)
+        {
+            foreach (var activation in _Activations)
+                activation?.RegisterActivationListener(onActivate);
+        }
+
+        public void UnregisterActivationListener(Action onActivate)
+        {
+            foreach (var activation in _Activations)
+                activation?.UnregisterActivationListener(onActivate);
+        }
+    }
+}
