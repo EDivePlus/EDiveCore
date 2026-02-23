f0f45f76b0ac025d508b186a4e483d72612e8c6d
František Holubec <frantisek.holubec@gmail.com>
2026-02-23 13:51:12 +0100

    Stage play fix ownership

--- StagePlay/NetworkStagePlayController.cs
+++ StagePlay/NetworkStagePlayController.cs
@@ -12,12 +12,12 @@ namespace EDIVE.StagePlay
     public class NetworkStagePlayController : NetworkBehaviour
     {
         private StagePlayController _controller;
-        
+
         private readonly SyncVar<StagePlayDefinition> _definition = new();
         private readonly SyncVar<int> _currentSegment = new();
-        
+
         private StagePlayState _currentState;
-        
+
         private void Awake()
         {
             _controller = GetComponent<StagePlayController>();
@@ -26,46 +26,46 @@ namespace EDIVE.StagePlay
         public override void OnStartClient()
         {
             base.OnStartClient();
-            
+
             OnLocalDefinitionChanged(_controller.Definition, _controller.CurrentState);
             _controller.DefinitionChanged += OnLocalDefinitionChanged;
-            
+
             _definition.OnChange += OnSyncDefinitionChanged;
             _currentSegment.OnChange += OnSyncCurrentSegmentChanged;
         }
-        
+
         public override void OnStopClient()
         {
             base.OnStopClient();
             _controller.DefinitionChanged -= OnLocalDefinitionChanged;
-            
+
             _definition.OnChange -= OnSyncDefinitionChanged;
             _currentSegment.OnChange -= OnSyncCurrentSegmentChanged;
         }
-        
+
         private void OnLocalDefinitionChanged(StagePlayDefinition definition, StagePlayState state)
         {
             SetDefinition(definition);
             SetCurrentSegment(state.CurrentSegmentIndex);
-            
+
             if (_currentState != null)
                 _currentState.CurrentSegmentChanged -= OnLocalCurrentSegmentChanged;
             _currentState = state;
             _currentState.CurrentSegmentChanged += OnLocalCurrentSegmentChanged;
         }
-        
+
         private void OnLocalCurrentSegmentChanged(int segment)
         {
             SetCurrentSegment(segment);
         }
-        
-        [ServerRpc]
+
+        [ServerRpc(RequireOwnership = false)]
         private void SetDefinition(StagePlayDefinition definition)
         {
             _definition.Value = definition;
         }
         
-        [ServerRpc]
+        [ServerRpc(RequireOwnership = false)]
         private void SetCurrentSegment(int segment)
         {
             _currentSegment.Value = segment;
