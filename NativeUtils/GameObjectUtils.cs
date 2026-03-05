9dda727c5725decb6f6a3548f695a1d96064a4ab
František Holubec <frantisek.holubec@gmail.com>
2026-03-05 09:34:05 +0100

    Gameobject utils

---
+++ NativeUtils/GameObjectUtils.cs
@@ -0,0 +1,23 @@
+﻿// Author: František Holubec
+// Created: 05.03.2026
+
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+namespace EDIVE.NativeUtils
+{
+    public static class GameObjectUtils
+    {
+        public static T Instantiate<T>(T prefab, Transform parent) where T : Object
+        {
+#if UNITY_EDITOR
+            if (!Application.isPlaying)
+                return (T) PrefabUtility.InstantiatePrefab(prefab, parent);
+#endif
+            return Object.Instantiate(prefab, parent);
+        }
+    }
+}
