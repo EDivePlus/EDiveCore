a270cff126263c12e48721956902417bb8271522
Michal Petr <jeerdus55@gmail.com>
2026-03-05 11:23:53 +0100

    feat: Font Symbol visual presets

---
+++ Utils/FontSymbols/VisualPresets/FontSymbolVisualID.cs
@@ -0,0 +1,12 @@
+﻿// Author: Michal Petr
+// Created: 05.03.2026
+
+using EDIVE.VisualPresets.VisualIDs;
+
+namespace EDIVE.Utils.FontSymbols
+{
+    public class FontSymbolVisualID : ABaseVisualID
+    {
+        
+    }
+}
---
+++ Utils/FontSymbols/VisualPresets/FontSymbolVisualPreset.cs
@@ -0,0 +1,76 @@
+﻿// Author: Michal Petr
+// Created: 29.10.2025
+
+using System;
+using EDIVE.Utils.FontSymbols;
+using EDIVE.VisualPresets.Presets;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Scripting;
+
+namespace EDIVE.VisualPresets.Switchers
+{
+    [Serializable]
+    public class FontSymbolVisualPresetRecord : AVisualPresetRecord<FontSymbolVisualID>
+    {
+        [VerticalGroup("Value")]
+        [SerializeField]
+        private FontSymbol _FontSymbol;
+        
+        public FontSymbol FontSymbol
+        {
+            get => _FontSymbol;
+            set => _FontSymbol = value;
+        }
+        
+        public override string EditorLabel => "Font Symbol";
+    }
+    
+    [Serializable]
+    public class TextFontSymbolVisualSwitcherRecord : AVisualSwitcherRecord<FontSymbolVisualID>
+    {
+        [VerticalGroup("Value")]
+        [SerializeField]
+        private FontSymbolText _Text;
+        public FontSymbolText Text => _Text;
+        
+        public override string EditorLabel => "Text FontSymbol";
+        public override Type EditorIconTargetType => typeof(FontSymbolText);
+    }
+    
+    [Preserve]
+    public class TextFontSymbolVisualSwitcherStrategy : AVisualSwitcherStrategy<FontSymbolVisualID, FontSymbolVisualPresetRecord, TextFontSymbolVisualSwitcherRecord>
+    {
+        protected override void Apply(FontSymbolVisualPresetRecord presetRecord, TextFontSymbolVisualSwitcherRecord switcherRecord)
+        {
+            if (switcherRecord.Text == null) 
+                return;
+            
+            switcherRecord.Text.FontSymbol = presetRecord.FontSymbol;
+        }
+    }
+    
+    [Serializable]
+    public class TMPTextFontSymbolVisualSwitcherRecord : AVisualSwitcherRecord<FontSymbolVisualID>
+    {
+        [VerticalGroup("Value")]
+        [SerializeField]
+        private FontSymbolTMPTextUI _Text;
+        public FontSymbolTMPTextUI Text => _Text;
+        
+        public override string EditorLabel => "TMP FontSymbol";
+        public override Type EditorIconTargetType => typeof(FontSymbolTMPTextUI);
+    }
+    
+    [Preserve]
+    public class TMPTextFontSymbolVisualSwitcherStrategy : AVisualSwitcherStrategy<FontSymbolVisualID, FontSymbolVisualPresetRecord, TMPTextFontSymbolVisualSwitcherRecord>
+    {
+        protected override void Apply(FontSymbolVisualPresetRecord presetRecord, TMPTextFontSymbolVisualSwitcherRecord switcherRecord)
+        {
+            if (switcherRecord.Text == null) 
+                return;
+            
+            switcherRecord.Text.FontSymbol = presetRecord.FontSymbol;
+        }
+    }
+}
