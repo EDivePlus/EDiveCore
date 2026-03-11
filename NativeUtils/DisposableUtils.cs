974494324c24807666665c57ca3d2a7ded1c3880
Michal Petr <jeerdus55@gmail.com>
2026-03-11 14:35:03 +0100

    feat: Disposable Utils

---
+++ NativeUtils/DisposableUtils.cs
@@ -0,0 +1,31 @@
+﻿// Author: Michal Petr
+// Created: 11.03.2026
+
+using System;
+
+namespace EDIVE.NativeUtils
+{
+    public static class DisposableUtils
+    {
+        public static readonly IDisposable Empty = new EmptyDisposable();
+        
+        public static IDisposable Create(Action onDisposed)
+        {
+            return new AnonymousDisposable(onDisposed);
+        }
+    }
+    
+    internal sealed class EmptyDisposable : IDisposable
+    {
+        public void Dispose() { }
+    }
+        
+    internal sealed class AnonymousDisposable : IDisposable
+    {
+        private readonly Action _onDisposed;
+
+        public AnonymousDisposable(Action onDisposed) => _onDisposed = onDisposed;
+
+        public void Dispose() => _onDisposed?.Invoke();
+    }
+}
