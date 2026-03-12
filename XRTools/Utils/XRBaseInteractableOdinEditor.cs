2c880ccfcd4eb6aeaf6dcc3ad8fd0abbc8a0c780
František Holubec <frantisek.holubec@gmail.com>
2026-03-12 11:16:03 +0100

    XRBaseInteractableOdinEditor

---
+++ XRTools/Utils/XRBaseInteractableOdinEditor.cs
@@ -0,0 +1,14 @@
+﻿// Author: František Holubec
+// Created: 09.03.2026
+
+#if UNITY_EDITOR
+using EDIVE.OdinExtensions.Editor;
+using UnityEditor;
+using UnityEngine.XR.Interaction.Toolkit.Interactables;
+
+namespace EDIVE.XRTools
+{
+    [CustomEditor(typeof(XRBaseInteractable), true)]
+    public class XRBaseInteractableOdinEditor : AutoNativeWrapperOdinEditor<XRBaseInteractable> { }
+}
+#endif
