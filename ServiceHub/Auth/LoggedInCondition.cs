cb865aa4d1c73890077994b1b497f2d59aced07c
Michal Petr <jeerdus55@gmail.com>
2026-05-05 13:21:30 +0200

    fix: Missing Serializable tag

--- ServiceHub/Auth/LoggedInCondition.cs
+++ ServiceHub/Auth/LoggedInCondition.cs
@@ -1,10 +1,12 @@
 ﻿// Author: Michal Petr
 // Created: 04.05.2026
 
+using System;
 using EDIVE.Conditions;
 
 namespace EDIVE.ServiceHub.Auth
 {
+    [Serializable]
     public class LoggedInCondition : ICondition
     {
         public bool Evaluate()
