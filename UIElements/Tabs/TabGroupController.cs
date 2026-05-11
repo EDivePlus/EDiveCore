1129397bfe1ea1fb195501e0a98ec5af156b6d1a
Michal Petr <jeerdus55@gmail.com>
2026-05-11 11:57:47 +0200

    feat: Tab controller

---
+++ UIElements/Tabs/TabGroupController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using UnityEngine;
+
+namespace EDIVE.UIElements.Tabs
+{
+    public class TabGroupController : MonoBehaviour
+    {
+        [SerializeField]
+        private TabHandler _DefaultTab;
+
+        [SerializeField]
+        [ListDrawerSettings(ShowFoldout = false, OnTitleBarGUI = "OnTabsTitleBarGUI")]
+        private List<TabHandler> _Tabs;
+
+        private void Start()
+        {
+            if (_DefaultTab) 
+                OnTabSelected(_DefaultTab);
+        }
+
+        private void OnEnable()
+        {
+            _Tabs.ForEach(tab => tab.Selected += OnTabSelected);
+        }
+
+        private void OnDisable()
+        {
+            _Tabs.ForEach(tab => tab.Selected -= OnTabSelected);
+        }
+
+        private void OnTabSelected(TabHandler selectedTab)
+        {
+            _Tabs.ForEach(tab => tab.SetActive(tab == selectedTab));
+        }
+
+#if UNITY_EDITOR
+        [UsedImplicitly]
+        private void OnTabsTitleBarGUI(InspectorProperty property)
+        {
+            if (SirenixEditorGUI.ToolbarButton(EditorIcons.Refresh))
+            {
+                _Tabs = GetComponentsInChildren<TabHandler>().ToList();
+                property.MarkSerializationRootDirty();
+            }
+        }
+#endif
+    }
+}
---
+++ UIElements/Tabs/TabHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using EDIVE.StateHandling.ToggleStates;
+using EDIVE.Utils.Activations;
+using UnityEngine;
+
+namespace EDIVE.UIElements.Tabs
+{
+    public class TabHandler : MonoBehaviour
+    {
+        [SerializeReference]
+        private IActivation _Activation;
+
+        [SerializeField]
+        private AToggleState _Toggle;
+
+        public event Action<TabHandler> Selected;
+
+        private void OnEnable()
+        {
+            _Activation?.RegisterActivationListener(OnButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            _Activation?.UnregisterActivationListener(OnButtonClicked);
+        }
+
+        private void OnButtonClicked()
+        {
+            Selected?.Invoke(this);
+        }
+
+        public void SetActive(bool state)
+        {
+            if(_Toggle) 
+                _Toggle.SetState(state);
+        }
+    }
+}
