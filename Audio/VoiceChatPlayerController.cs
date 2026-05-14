5352b2263f4980700effde70397a3b80b64d406e
František Holubec <frantisek.holubec@gmail.com>
2026-05-14 10:55:24 +0200

    VoiceChat player - dont reparent, only follow

--- Audio/VoiceChatPlayerController.cs
+++ Audio/VoiceChatPlayerController.cs
@@ -2,7 +2,6 @@
 // Created: 23.04.2025
 
 using Adrenak.UniVoice.Outputs;
-using EDIVE.Networking.Players;
 using UnityEngine;
 
 namespace EDIVE.Audio
@@ -10,17 +9,19 @@ namespace EDIVE.Audio
     public class VoiceChatPlayerController : MonoBehaviour
     {
         [SerializeField]
-        private NetworkPlayerController _PlayerController;
-        
-        [SerializeField]
         private Transform _PeerRoot;
         
+        private StreamedAudioSourceOutput _audioOutput;
         
         public void InitializePeerAudioOutput(StreamedAudioSourceOutput audioOutput)
         {
-            var outputTransform = audioOutput.transform;
-            outputTransform.SetParent(_PeerRoot); 
-            outputTransform.localPosition = Vector3.zero;
+            _audioOutput = audioOutput;
+        }
+
+        private void Update()
+        {
+            if (_audioOutput != null)
+                _audioOutput.transform.position = _PeerRoot.position;
         }
     }
 }
