3047d49af3a66ffee45189a83003818021f5b446
František Holubec <frantisek.holubec@gmail.com>
2026-05-26 19:22:54 +0200

    Vector3ScriptableVariable

---
+++ ScriptableArchitecture/Variables/Impl/Vector3ScriptableVariable.cs
@@ -0,0 +1,12 @@
+﻿// Author: František Holubec
+// Created: 26.05.2026
+
+using UnityEngine;
+
+namespace EDIVE.ScriptableArchitecture.Variables.Impl
+{
+    public class Vector3ScriptableVariable : AScriptableVariable<Vector3>
+    {
+
+    }
+}
