a68e37f6479c158cda666b6c6e72df9543484912
Michal Petr <jeerdus55@gmail.com>
2026-06-01 14:22:34 +0200

    feat: Close widget button

---
+++ MenuScreen/CloseWidgetButton.cs
@@ -0,0 +1,41 @@
+﻿// Author: Michal Petr
+// Created: 01.06.2026
+
+using EDIVE.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EDIVE.MenuScreen
+{
+    public class CloseWidgetButton : MonoBehaviour
+    {
+        [SerializeField]
+        [Required]
+        private Button _CloseButton;
+        
+        private void OnEnable()
+        {
+            if (_CloseButton)
+                _CloseButton.onClick.AddListener(OnCloseButtonClicked);
+        }
+
+        private void OnDisable()
+        {
+            if (_CloseButton)
+                _CloseButton.onClick.RemoveListener(OnCloseButtonClicked);
+        }
+        
+        private void OnCloseButtonClicked()
+        {
+            AppCore.Services.TryGet(out MenuScreenController controller);
+            if (controller == null)
+            {
+                Debug.LogError("MenuScreenController not found in scene. CloseWidgetButton requires MenuScreenController to function.");
+                return;
+            }
+            
+            controller.CollapseCurrentFrame();
+        }
+    }
+}
