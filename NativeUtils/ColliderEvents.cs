7d85d5a3a2cbdb6c794c84f534aa3f2644f939a8
František Holubec <frantisek.holubec@gmail.com>
2026-06-02 12:09:08 +0200

    Collider events

---
+++ NativeUtils/ColliderEvents.cs
@@ -0,0 +1,26 @@
+﻿// Author: František Holubec
+// Created: 04.09.2025
+
+using System;
+using UnityEngine;
+
+namespace EDIVE.NativeUtils
+{
+    public class ColliderEvents : MonoBehaviour
+    {
+        public event Action<Collider> TriggerEntered;
+        public event Action<Collider> TriggerExited;
+        public event Action<Collider> TriggerStayed;
+        
+        public event Action<Collision> ColliderEntered;
+        public event Action<Collision> ColliderExited;
+        public event Action<Collision> ColliderStayed;
+
+        private void OnTriggerEnter(Collider other) => TriggerEntered?.Invoke(other);
+        private void OnTriggerExit(Collider other) => TriggerExited?.Invoke(other);
+        private void OnTriggerStay(Collider other) => TriggerStayed?.Invoke(other);
+        private void OnCollisionEnter(Collision other) => ColliderEntered?.Invoke(other);
+        private void OnCollisionExit(Collision other) => ColliderExited?.Invoke(other);
+        private void OnCollisionStay(Collision other) => ColliderStayed?.Invoke(other);
+    }
+}
