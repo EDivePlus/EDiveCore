cf35f06ca4676f849f1c5176e10992b5bf9f7449
František Holubec <frantisek.holubec@gmail.com>
2026-06-11 15:28:51 +0200

    MapCoordinatesHelper - clamp pos and sample height

--- GeoToolkit/Helpers/MapCoordinatesHelper.cs
+++ GeoToolkit/Helpers/MapCoordinatesHelper.cs
@@ -17,6 +17,13 @@ namespace EDIVE.GeoToolkit.Maps
         private MapController _Map;
 
         [SerializeField]
+        private bool _ClampPositionToMap;
+
+        [EnableIf(nameof(_ClampPositionToMap))]
+        [SerializeField]
+        private bool _SampleMapHeight;
+
+        [SerializeField]
         private TMP_Text _Text;
 
         [SerializeField]
@@ -43,6 +50,9 @@ namespace EDIVE.GeoToolkit.Maps
 
                 var pos = _Map.ConvertToMapCoordinates(value);
                 transform.position = transform.position.WithXZ(pos.x, pos.z);
+
+                if (_ClampPositionToMap)
+                    ClampPositionToMap(_SampleMapHeight);
             }
         }
 
@@ -52,6 +62,28 @@ namespace EDIVE.GeoToolkit.Maps
             {
                 _Text.text = GeoCoordinates.Position.ToString();
             }
+        }
+
+        private void LateUpdate()
+        {
+            if (_ClampPositionToMap)
+                ClampPositionToMap(_SampleMapHeight);
+        }
+
+        [Button]
+        public void ClampPositionToMap(bool sampleHeight = true)
+        {
+            if (_Map == null)
+                return;
+
+            // Clamp the position back inside the map area by round-tripping through clamped geo coordinates.
+            var clampedGeo = _Map.ConvertToGeoCoordinates(transform.position, clamp: true);
+            var mapPos = _Map.ConvertToMapCoordinates(clampedGeo, sampleHeight);
+
+            // When sampling height, snap fully onto the sampled surface point; otherwise only clamp the XZ plane.
+            transform.position = sampleHeight
+                ? (Vector3) mapPos
+                : transform.position.WithXZ(mapPos.x, mapPos.z);
         }
     }
 }
