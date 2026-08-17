241f2b1150f26af34739fe85cf100297640da9bc
Michal Petr <jeerdus55@gmail.com>
2026-08-17 15:13:20 +0200

    feat: Collider state presets

---
+++ StateHandling/StateValuePresets/ColliderPresets.cs
@@ -0,0 +1,17 @@
+﻿// Author: Michal Petr
+// Created: 17.08.2026
+
+using System;
+using Newtonsoft.Json;
+using UnityEngine;
+
+namespace EDIVE.StateHandling.StateValuePresets
+{
+    [Serializable, JsonObject(MemberSerialization.OptIn)]
+    public class ColliderEnabledPreset : AStateValuePreset<Collider, bool>
+    {
+        public override string Title => "Enabled";
+        public override void ApplyTo(Collider targetObject) => targetObject.enabled = Value;
+        public override void CaptureFrom(Collider targetObject) => Value = targetObject.enabled;
+    }
+}
