3b731377604d69879ef5f4a931cf2f033697b9bb
Michal Petr <jeerdus55@gmail.com>
2026-09-24 15:50:20 +0200

    chore: Add EnhancedTypeSelector to IAction

--- Utils/Actions/IAction.cs
+++ Utils/Actions/IAction.cs
@@ -2,9 +2,11 @@
 // Created: 22.09.2026
 
 using Cysharp.Threading.Tasks;
+using EDIVE.OdinExtensions.Attributes;
 
 namespace EDIVE.Utils.Actions
 {
+    [EnhancedTypeSelector(true, 1)]
     public interface IAction
     {
         UniTask Execute();
